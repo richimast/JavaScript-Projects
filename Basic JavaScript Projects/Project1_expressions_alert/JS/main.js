a35598d9fd4109d2a06f47b73352c96bf3330617
richimast <richimast@gmail.com>
2026-07-15 14:37:34 +0100

    Project1_expressions_alert Assignment
    This is my Project1_expressions_Assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -8,27 +8,33 @@ document.write('Hello, world!');
 var A = "This is a string";
 //This is used to display the string assign to the variable i wrote to it 
 document.write(A);
+document.write("<br>");
 
 //This is for escaping characters 
 document.write("Lisa told Bart, \"Knock it off, Bart! or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
+document.write("<br>");
 
 //This is used to Concatenate Strings together by using + operator
 document.write("\"Be who you are and say what you feel,"
     + " because those who mind don\'t mattter and those who matter don\'t mind.\""
     + "-Dr. Seuss.\"");
+document.write("<br>");
 
 //creating variable and concatenate string
 var B = "Concatenated" + "String"
 //displaying variable using document.write method
 document.write(B);
+document.write("<br>");
 
 //This is used assign multiple variables in one statement 
 var Family = "The Arezzinis", Dad = "Richard", Mom = "Vida", Daughter = "Anita", Son = "Eric";
 document.write(Dad);
+document.write("<br>");
 
 //This is used to display expression and statement
 3 + 5
 document.write(3 + 5);
+document.write("<br>");
 
 //main.js
 //window.alert method
@@ -36,6 +42,7 @@ window.alert("Hello, world!");
 
 //document.wirte method
 document.write("For the last time, hello, world!");
+document.write("<br>");
 
 //creating two variables
 var Sent1 = "This is the beginning of the string";
@@ -43,6 +50,7 @@ var Sent2 = "and this is the end of the string";
 
 //displaying the variables using the document.write method
 document.write(Sent1 + Sent2);
+document.write("<br>");
 
 //an expression
 document.write(30 * 25);
